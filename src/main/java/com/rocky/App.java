409c73d10991aeee56b3d2d342bf2e3c47ffc0ea
Rocky Majumdar <rockymjr@gmail.com>
2019-12-17 23:30:30 +0530

    New project created

---
+++ src/main/java/com/rocky/App.java
@@ -0,0 +1,13 @@
+package com.rocky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
